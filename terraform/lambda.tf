48a0405b90f1d06f981586ff16c8821bea4a8472
Robert Riches <robert.richesjr@infor.com>
2023-03-31 11:12:56 -0700

    First attempt to have TF use the container image rather than the ZIP file.

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,10 +1,11 @@
 resource "aws_lambda_function" "token_lambda" {
   function_name = "${var.service_name}-create-token"
-  filename = "${path.module}/lambda_package.zip"
   handler = "token_lambda.handler"
-  runtime = "python3.9"
-  source_code_hash = filebase64sha256("${path.module}/lambda_package.zip")
   role = aws_iam_role.lambda_role.arn
+
+  create_package = false
+  image_uri = "ghcr.io/martian-cloud/iam-oidc-provider/iam-oidc-provider-token:latest"
+  package_type = "Image"
 
   environment {
     variables = {
@@ -25,11 +26,12 @@ resource "aws_lambda_permission" "apigw_token_lambda_permission" {
 
 resource "aws_lambda_function" "oidc_lambda" {
   function_name = "${var.service_name}-discovery"
-  filename = "${path.module}/lambda_package.zip"
   handler = "oidc_lambda.handler"
-  runtime = "python3.9"
-  source_code_hash = filebase64sha256("${path.module}/lambda_package.zip")
   role = aws_iam_role.lambda_role.arn
+
+  create_package = false
+  image_uri = "ghcr.io/martian-cloud/iam-oidc-provider/iam-oidc-provider-oidc:latest"
+  package_type = "Image"
 
   environment {
     variables = {
